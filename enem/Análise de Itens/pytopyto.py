2022a2e1ead16da04c3ba7359c3672783e218f68
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2023-02-25 12:27:51 -0300

    z

--- enem/Análise de Itens/pytopyto.py
+++ enem/Análise de Itens/pytopyto.py
@@ -1,32 +1,48 @@
 import numpy as np
 from scipy.optimize import minimize_scalar
 
-def calculate_likelihood(theta, a, b, c, x):
+def calcular_probabilidade(theta, a, b, c):
+    """
+    Calcula a probabilidade de uma resposta correta dado o nível de proficiência de um candidato e os parâmetros do item.
+
+    Parâmetros:
+        theta (float): Nível de proficiência do candidato.
+        a (float): Parâmetro de discriminação.
+        b (float): Parâmetro de dificuldade.
+        c (float): Parâmetro de acerto ao acaso.
+
+    Retorna:
+        Probabilidade de uma resposta correta.
+    """
+    return c + (1 - c) * (1 / (1 + np.exp(-a * (theta - b))))
+
+def calcular_verossimilhanca(theta, a, b, c, x):
     # theta: valor da proficiência do candidato
     # a, b, c: parâmetros dos itens
     # x: matriz de respostas dos candidatos (0 para erro e 1 para acerto)
-    
-    num_items, num_candidates = x.shape
-    likelihood = np.ones(num_candidates)
-    
-    for i in range(num_items):
-        p = c[i] + (1 - c[i]) / (1 + np.exp(-a[i] * (theta - b[i])))
-        likelihood *= np.power(p, x[i, :]) * np.power(1 - p, 1 - x[i, :])
-        
-    return np.prod(likelihood)
+    if x.ndim == 1:
+        x = np.expand_dims(x, axis=1)
+    num_itens, num_candidatos = x.shape
+    verossimilhanca = np.ones(num_candidatos)
+    for i in range(num_itens):
+        p = calcular_probabilidade(theta, a[i], b[i], c[i])
+        verossimilhanca *= np.power(p, x[i]) * np.power(1-p, 1-x[i])
+    return np.prod(verossimilhanca)
 
 # Define a função de verossimilhança como uma função de theta
-def likelihood(theta, a, b, c, x):
-    return -calculate_likelihood(theta, a, b, c, x)  # O sinal negativo é usado porque o objetivo é maximizar a função
+def verossimilhanca(theta, a, b, c, x):
+    return -calcular_verossimilhanca(theta, a, b, c, x.flatten())  # O sinal negativo é usado porque o objetivo é maximizar a função
 
 # Define os parâmetros dos itens e a matriz de respostas dos candidatos
-a = [1.0, 1.5, 2.0]
-b = [-1.0, 0.0, 1.0]
-c = [0.1, 0.2, 0.3]
+a = [2.53465, 2.5914, 1.41618]
+b = [2.4095, 1.40589, 0.30941]
+c = [0.0954, 0.13998, 0.07884]
 x = np.array([[1, 0, 1], [0, 1, 1], [1, 1, 0]])
 
-# Encontra o valor de theta que maximiza a função de verossimilhança usando o método de Brent
-result = minimize_scalar(likelihood, args=(a, b, c, x), bounds=(-10.0, 10.0), method='bounded')
-theta_max = ((result.x*100)+500)
+# Encontra o valor de theta que maximiza a função de verossimilhança para cada candidato usando o método de Brent
+theta_max_list = []
+for i in range(x.shape[1]):
+    result = minimize_scalar(verossimilhanca, args=(a, b, c, x[:, i]), bounds=(-4, 4), method='bounded')
+    theta_max_list.append((result.x*100)+500)
 
-print(f"Valor de theta que maximiza a função de verossimilhança: {theta_max}")
+print(f"Valores de theta que maximizam a função de verossimilhança para cada candidato: {theta_max_list}")
