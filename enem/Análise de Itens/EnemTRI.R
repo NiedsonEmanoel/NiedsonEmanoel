4105fd1017efcf270f12c253dd9694e0ce58a844
Niedson Emanoel <niedsonemanoeltbm@gmail.com>
2023-11-29 15:57:24 -0300

    r

---
+++ enem/Análise de Itens/EnemTRI.R
@@ -0,0 +1,23 @@
+library(mirt)
+
+#Lê os dados do arquivo
+dados <- read.delim("F:/Niedson Emanoel/Desktop/dados.txt")
+View(dados)
+
+#Ajusta o modelo TRI 3PL
+mod3 <- mirt(dados, 1, itemtype = '3PL')
+coef(mod3, simplify=TRUE, IRTpars=TRUE)
+
+#Extrai os coeficientes e salva em um data frame
+#coeficientes <- coef(mod3, simplify=TRUE, IRTpars=TRUE)
+
+#Especifica o caminho do arquivo CSV de saída
+#caminho_saida <- "F:/Niedson Emanoel/Desktop/resultados_coef.csv"
+
+#Escreve os coeficientes no arquivo CSV
+#write.csv(coeficientes, file = caminho_saida, row.names = FALSE)
+
+#Plots e outras análises...
+plot(mod3)
+plot(mod3, type='trace')
+plot(mod3, type='infotrace')
